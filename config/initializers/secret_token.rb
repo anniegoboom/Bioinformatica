1d4893a9dd58698a59ef4a359c73305267a9f808
Annie Zhou <annie@lumoslabs.com>
2014-05-05 16:45:16 -0700

    creating the rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bioinformatica::Application.config.secret_token = '5926aa9b4079c6d385aa0ab26b89649e9afeb4517e54193ae52927dd3fb9d24c34f78e73e036e938c16b141618eb33986cefe87ff6d21d22bfa8931ecad98952'
