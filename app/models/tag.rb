d7c49231e3baa92399d146a84c61c2fe394beb37
Annie Zhou <annie@lumoslabs.com>
2014-05-14 01:44:16 -0700

    adding in searching through tags by type

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -11,4 +11,24 @@ class Tag < ActiveRecord::Base
   validates_associated :info_snippets
 
   attr_accessible :tag, :tag_type_id
+
+  def self.find_or_create_new(name, type)
+    tag_type = TagType.find_by_name(type) #TODO: blow up if type not found
+    tag = Tag.find_by_name_and_type(name, tag_type) || Tag.create(tag: name, tag_type_id: tag_type.id)
+  end
+
+  def self.find_by_name_and_type(name, tag_type)
+    tags_array = tag_type.tags
+    tags_array.each do |tag|
+      return tag if equivalent_names(tag.tag, name)
+    end
+    nil
+  end
+
+  private
+
+  #TODO: add similar tag name checks in the future
+  def self.equivalent_names(tag_name, name)
+    tag_name.downcase == name.downcase
+  end
 end
