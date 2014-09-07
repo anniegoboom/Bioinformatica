62123ae48f4c481b4ca1ad02cb14d3b77192d9fe
Annie Zhou <annie@lumoslabs.com>
2014-09-06 19:07:56 -0700

    changed all edit/delete links to buttons

--- app/controllers/bioinformatica_controller.rb
+++ app/controllers/bioinformatica_controller.rb
@@ -3,42 +3,4 @@ class BioinformaticaController < ApplicationController
 
   def index
   end
-
-  # def new
-  # end
-
-  # def index
-  #   @info_snippets = InfoSnippet.all
-  # end
-
-  # def create
-  #   @info = InfoSnippet.new(info_params)
-
-  #   @info.save
-  #   render :new
-  # end
-
-  # def show
-  #   @info = InfoSnippet.find(params[:id])
-  # end
-
-  # def edit
-  #   @info = InfoSnippet.find(params[:id])
-  # end
-
-  # def update
-  #   @info = InfoSnippet.find(params[:id])
-
-  #   if @info.update_attributes(info_params)
-  #     render :show
-  #   else
-  #     render :edit
-  #   end
-  # end
-
-  # private
-
-  # def info_params
-  #   params.require(:info_snippet).permit(:text)
-  # end
 end
