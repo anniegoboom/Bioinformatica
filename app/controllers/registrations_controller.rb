1a044484517f41f83689d29620cfbd136004c382
Annie Zhou <annie@lumoslabs.com>
2014-09-08 02:34:13 -0700

    flash messages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
   def new
-    flash[:info] = 'Registrations are not allowed, please contact joe or annie'
+    flash[:notice] = 'Registrations are not allowed, please contact joe or annie'
     redirect_to root_path
   end
 
   def create
-    flash[:info] = 'Registrations are not allowed, please contact joe or annie'
+    flash[:notice] = 'Registrations are not allowed, please contact joe or annie'
     redirect_to root_path
   end
 end
